96812f0727c36e898972877a666f17450b3dafec
mlonneman <mlonneman@gmail.com>
2020-01-16 10:37:05 -0500

    development of numeric QA tests

--- documents/development_script.R
+++ documents/development_script.R
@@ -41,6 +41,64 @@ protocol_minimums <- protocol_structure %>%
 
 df_numeric$depth_m <- c(-5, -8, 1)
 
+# seq along columns :
 # Numeric test 
-df_numeric$depth_m < filter(protocol_minimums, attribute_name == "depth_m")$minimum_warning
+# Which values are less than the minimum
+results <- df_numeric$depth_m < filter(protocol_minimums, attribute_name == "depth_m")$minimum_warning
+which(results %in% TRUE) 
+if(length(which(results %in% TRUE)))
+  
+  
+df_numeric$depth_m < filter(protocol_minimums, attribute_name == "depth_m")$maximum_warning
 which((test < 0) %in% 1) 
+
+
+paste(as.character(df_numeric[minimum_results, "depth_m"]), collapse=", ")
+
+####
+protocol_minmax <- protocol_structure %>%
+  filter(protocol == current_protocol & sheet == sheet_name & attribute_name %in% colnames(testing_df))
+
+for(column in testing_df){
+  
+  ## ... Minimum Test ####
+  # Find the index for any values that are less than the attribute's minimum
+  min_invalid_row_index <- which(testing_df[column] < filter(protocol_minmax, attribute_name == column)$minimum_warning 
+        %in% 1) 
+  # If there are invalid values:
+  if(length(min_invalid_row_index) > 0){
+    # Extract invalid value at each index
+    invalid_values <- pull(testing_df[column])[min_invalid_row_index]
+    
+    numeric_results <- setNames(as.data.frame(paste(min_invalid_row_index, collapse = ", ")), "row_numbers") %>%
+      mutate(column_name = column,
+             sheet_name = sheet_name,
+             protocol = current_protocol,
+             test = "Invalid minimum value",
+             filename = filenames[i],
+             values = paste(invalid_values, collapse=", ")) %>%
+      select(test, filename, protocol, sheet_name, column_name, row_numbers, values) %>%
+      bind_rows(numeric_results)
+  }
+  
+  ## ... Maximum Test ####
+  # Find the index for any values that are greater than the attribute's maximum
+  max_invalid_row_index <- which(testing_df[column] > filter(protocol_minmax, attribute_name == column)$maximum_warning 
+                             %in% 1) 
+  # If there are invalid values:
+  if(length(max_invalid_row_index) > 0){
+    # Extract invalid value at each index
+    invalid_values <- pull(testing_df[column])[max_invalid_row_index]
+    
+    numeric_results <- setNames(as.data.frame(paste(invalid_row_index, collapse = ", ")), "row_numbers") %>%
+      mutate(column_name = column,
+             sheet_name = sheet_name,
+             protocol = current_protocol,
+             test = "Invalid maximum value",
+             filename = filenames[i],
+             values = paste(invalid_values, collapse=", ")) %>%
+      select(test, filename, protocol, sheet_name, column_name, row_numbers, values) %>%
+      bind_rows(numeric_results)
+  }
+  
+}
